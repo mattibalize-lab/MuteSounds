001d16e80da0ccb4ab9a2d5348ce39168f0908e4
Matthew Rodrigues <mattcapazz@gmail.com>
2023-09-29 07:21:42 +0100

    init!

---
+++ app.js
@@ -0,0 +1,68 @@
+const fs = require("fs"),
+  file = "sounds.txt",
+  os = require("os"),
+  path = require("path");
+const axios = require("axios");
+
+if (fs.existsSync("Sound")) fs.rmSync("Sound", { recursive: true });
+fs.readFile(file, "utf8", (err, data) => {
+  if (err) return console.error(err);
+  if (!data)
+    return console.error(`File ${b}sounds.txt${r} is empty or doesn't exist!`);
+
+  const eol = os.EOL,
+    sep = path.sep,
+    b = "\x1b[1m",
+    r = "\x1b[0m",
+    time = Date.now();
+
+  const sounds = data.split(eol);
+
+  const promises = [];
+  sounds.forEach((sound) => {
+    if (!sound) return sounds.splice(sounds.indexOf(sound), 1);
+
+    const url = `https://wow.tools/files/scripts/api.php?draw=38&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=5&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=6&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=25&search%5Bvalue%5D=${sound}%2Ctype%3Aogg&search%5Bregex%5D=false&_=1695962035525`;
+
+    promises.push(
+      axios
+        .get(url)
+        .then((res) => {
+          let filePath = res.data["data"][0][1].replace(/\//g, sep);
+          filePath = filePath.charAt(0).toUpperCase() + filePath.slice(1);
+          const dirPath = path.dirname(filePath);
+
+          if (!fs.existsSync(dirPath))
+            fs.mkdirSync(dirPath, { recursive: true });
+
+          fs.writeFileSync(filePath, "");
+          fs.writeFileSync(
+            filePath.slice(0, -3) + "wav",
+            "RIFF$   WAVEfmt \u0010   \u0001 \u0001 D¬  ˆX\u0001 \u0002 \u0010 data\r\n"
+          ); // empty .wav file
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+    );
+  });
+
+  Promise.all(promises)
+    .then(() => {
+      console.log(`Done in ${Date.now() - time}ms!`);
+
+      console.log(`Sounds muted (${sounds.length}):`);
+      console.log(sounds);
+
+      console.log(`\nMove the generated ${b}Sound${r} folder to ${b}WoW_Client${sep}Data${sep}enUS${sep}${r}
+
+${b}Note${r}: You might have a different language folder than ${b}enUS${r}
+if so, just move the ${b}Sound${r} folder to your language folder.
+
+${b}If you experience any issues, report them on the GitHub repository:
+https://github.com/mattibalize-lab/MuteSounds`);
+    })
+    .catch((err) => {
+      return console.error(err);
+    });
+});
